d04f444474e21076006a0467c18464ad07531f1b
Paul Jacobson <pljcbsn@gmail.com>
2019-08-09 12:21:32 +0300

    Formatting the doc with black

--- pmapp/__init__.py
+++ pmapp/__init__.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'pmapp.sqlite'),
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "pmapp.sqlite")
     )
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -23,7 +23,8 @@ def create_app(test_config=None):
         pass
 
     # A simple page that says Hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
+
     return app
