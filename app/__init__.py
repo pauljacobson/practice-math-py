a5805e8bcc32a5ecede6d52a32ed450c4cad4f4f
Paul Jacobson <pljcbsn@gmail.com>
2023-12-09 12:56:12 +0200

    Imported SQLAlchemy and configured the database URI

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
+# Import the routes after the Flask app is created
 from app import routes
