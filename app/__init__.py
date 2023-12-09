5d7aaec61a695763b98c75880dd3a125d0f26065
Paul Jacobson <pljcbsn@gmail.com>
2023-12-09 13:46:51 +0200

    Refactor app initialization and create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# Import the routes after the Flask app is created
-from app import routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    db.init_app(app)
+
+    # Import the routes after the Flask app is created
+    from app import routes
+
+    app.register_blueprint(routes.bp)
+
+    return app
