0508be958a172ae1f77dcce42a50bc31bd86865d
Paul Jacobson <pljcbsn@gmail.com>
2023-12-09 12:56:37 +0200

    Add loading indicator and update message display

--- app/templates/chat.js
+++ app/templates/chat.js
@@ -1,18 +1,47 @@
+/**
+ * Establishes a connection to the server.
+ * @type {Socket}
+ */
 var socket = io();
 
+/**
+ * Sends a message to the server and shows the loading indicator.
+ */
 function sendMessage() {
   var text = document.getElementById("message-input").value;
   socket.emit("message", { text: text });
 
   // Show the loading indicator
-  document.getElementById("loading").style.display = "block";
+  showLoadingIndicator();
 }
 
 // Listening for responses from the server
 socket.on("response", function (data) {
-  var messages = document.getElementById("messages");
-  messages.innerHTML += "<div>" + data.text + "</div>";
+  updateMessages(data.text);
 
   // Hide the loading indicator
-  document.getElementById("loading").style.display = "none";
+  hideLoadingIndicator();
 });
+
+/**
+ * Shows the loading indicator by changing its display style to 'block'.
+ */
+function showLoadingIndicator() {
+  document.getElementById("loading").style.display = "block";
+}
+
+/**
+ * Hides the loading indicator by changing its display style to 'none'.
+ */
+function hideLoadingIndicator() {
+  document.getElementById("loading").style.display = "none";
+}
+
+/**
+ * Updates the messages displayed on the page by appending a new message.
+ * @param {string} text - The text of the message to append.
+ */
+function updateMessages(text) {
+  var messages = document.getElementById("messages");
+  messages.innerHTML += "<div>" + text + "</div>";
+}
